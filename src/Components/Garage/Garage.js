678decfbb8db216bbacaeb283ffb356c06bae587
Aaron Estes <aestesc@gmail.com>
2019-04-02 14:02:11 -0500

    cleaned up numbers

--- src/Components/Garage/Garage.js
+++ src/Components/Garage/Garage.js
@@ -11,16 +11,16 @@ export default class Garage extends Component{
         
         svg.append('rect').attr('class', 'garage').attr('x', 20).attr('y', 20).attr('width', 500).attr('height', 380);
 
-        svg.append('rect').attr('class', 'squareSpace').attr('x', 245-180).attr('y', 40).attr('width', 420).attr('height', 270);
-        svg.append('rect').attr('class', 'groundTrim').attr('x', 245-180).attr('y', 305).attr('width', 420).attr('height', 5);
-        svg.append('rect').attr('class', 'door').attr('x', 550-180).attr('y', 80).attr('width', 100).attr('height', 225);
-        svg.append('circle').attr('class', 'doorHandle').attr('cx', 560-180).attr('cy', 200).attr('r', 5);
+        svg.append('rect').attr('class', 'squareSpace').attr('x', 65).attr('y', 40).attr('width', 420).attr('height', 270);
+        svg.append('rect').attr('class', 'groundTrim').attr('x', 65).attr('y', 305).attr('width', 420).attr('height', 5);
+        svg.append('rect').attr('class', 'door').attr('x', 370).attr('y', 80).attr('width', 100).attr('height', 225);
+        svg.append('circle').attr('class', 'doorHandle').attr('cx', 380).attr('cy', 200).attr('r', 5);
 
-        svg.append('line').attr('class', 'topLeft').attr('x1', 200-180).attr('y1', 20).attr('x2', 245-180).attr('y2', 40);
-        svg.append('line').attr('class', 'bottomLeft').attr('x1', 200-180).attr('y1', 400).attr('x2', 245-180).attr('y2', 310);
-        svg.append('line').attr('class', 'bottomRight').attr('x1', 700-180).attr('y1', 400).attr('x2', 665-180).attr('y2', 310);
-        svg.append('line').attr('class', 'topRight').attr('x1', 700-180).attr('y1', 20).attr('x2', 665-180).attr('y2', 40);
-        
+        svg.append('line').attr('class', 'topLeft').attr('x1', 20).attr('y1', 20).attr('x2', 65).attr('y2', 40);
+        svg.append('line').attr('class', 'bottomLeft').attr('x1', 20).attr('y1', 400).attr('x2', 65).attr('y2', 310);
+        svg.append('line').attr('class', 'bottomRight').attr('x1', 520).attr('y1', 400).attr('x2', 485).attr('y2', 310);
+        svg.append('line').attr('class', 'topRight').attr('x1', 520).attr('y1', 20).attr('x2', 485).attr('y2', 40);
+
         return(<p>How do I maximize the use of space given the following constraints:</p>)
     }
 }
