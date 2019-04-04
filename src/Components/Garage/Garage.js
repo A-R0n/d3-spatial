0c8cd10c00ad8d5177373c52c07fbc999b2bbc40
Aaron Estes <aestesc@gmail.com>
2019-04-03 23:01:43 -0500

    new path when container size is selected

--- src/Components/Garage/Garage.js
+++ src/Components/Garage/Garage.js
@@ -4,19 +4,24 @@ import './Garage.css';
 
 export default class Garage extends Component {
 	constructor() {
-        super();
-        
-        this.state = {
-            value: 'small'
-        }
-    }
+		super();
 
-    componentDidMount(){
+		this.state = {
+            value: 'small',
+            bottomPoint: null
+		};
+	}
+
+	componentDidMount() {
+        this.drawGarage();
+    };
+
+    drawGarage = () => {
         const margin = { top: 20, right: 20, bottom: 20, left: 20 };
 		const width = 580 - margin.left - margin.right;
 		const height = 460 - margin.top - margin.bottom;
 		var svg = d3.select('body').append('svg').attr('class', 'house').attr('width', width).attr('height', height);
-
+        // const drag = d3.drag().on('start', started).on('drag', dragged).on('end', ended);
 		svg.append('rect').attr('class', 'garage').attr('x', 20).attr('y', 20).attr('width', 500).attr('height', 380);
 
 		svg
@@ -28,13 +33,7 @@ export default class Garage extends Component {
 			.attr('height', 270);
 
 		var countainerMount = d3.select('.squareSpace').call(draw);
-		svg
-			.append('rect')
-			.attr('class', 'groundTrim')
-			.attr('x', 65)
-			.attr('y', 305)
-			.attr('width', 420)
-			.attr('height', 5);
+		svg.append('rect').attr('class', 'groundTrim').attr('x', 65).attr('y', 305).attr('width', 420).attr('height', 5);
 		svg.append('rect').attr('class', 'door').attr('x', 370).attr('y', 80).attr('width', 100).attr('height', 225);
 
 		svg.append('line').attr('class', 'topLeft').attr('x1', 20).attr('y1', 20).attr('x2', 65).attr('y2', 40);
@@ -45,14 +44,7 @@ export default class Garage extends Component {
 		svg.append('line').attr('class', 'doorOpenEdge').attr('x1', 462).attr('y1', 82).attr('x2', 465).attr('y2', 83);
 		svg.append('line').attr('class', 'doorOpenEdge').attr('x1', 462).attr('y1', 82).attr('x2', 462).attr('y2', 300);
 		svg.append('line').attr('class', 'doorOpenEdge').attr('x1', 465).attr('y1', 83).attr('x2', 465).attr('y2', 293);
-		svg
-			.append('line')
-			.attr('class', 'doorOpenEdge')
-			.attr('x1', 462)
-			.attr('y1', 300)
-			.attr('x2', 465)
-			.attr('y2', 293);
-
+		svg.append('line').attr('class', 'doorOpenEdge').attr('x1', 462).attr('y1', 300).attr('x2', 465).attr('y2', 293);
 		svg.append('line').attr('id', 'doorFrame').attr('x1', 415).attr('y1', 88).attr('x2', 415).attr('y2', 265);
 		svg.append('line').attr('id', 'doorFrame').attr('x1', 415).attr('y1', 88).attr('x2', 462).attr('y2', 82);
 		svg.append('line').attr('id', 'doorFrame').attr('x1', 415).attr('y1', 265).attr('x2', 462).attr('y2', 300);
@@ -70,78 +62,28 @@ export default class Garage extends Component {
 
 		svg.append('line').attr('id', 'backbackdrop').attr('x1', 370).attr('y1', 145).attr('x2', 415).attr('y2', 145);
 
-		svg
-			.append('image')
-			.attr('href', 'https://image.flaticon.com/icons/svg/1519/1519002.svg')
-			.attr('id', 'fishTank')
-			.attr('x', 370)
-			.attr('y', 100);
-		svg
-			.append('image')
-			.attr('href', 'https://image.flaticon.com/icons/svg/1661/1661318.svg')
-			.attr('id', 'fishTable')
-			.attr('x', 370)
-			.attr('y', 117);
-		svg
-			.append('image')
-			.attr(
-				'href',
-				'https://i1.wp.com/teameverlast.everlast.com/wp-content/uploads/2016/07/2000px-Everlast-logo-2011.svg_.png?resize=300%2C197&ssl=1'
-			)
-			.attr('id', 'fishTable')
-			.attr('x', 95)
-            .attr('y', 190);
+		svg.append('image').attr('href', 'https://image.flaticon.com/icons/svg/1519/1519002.svg').attr('id', 'fishTank').attr('x', 370).attr('y', 100);
+		svg.append('image').attr('href', 'https://image.flaticon.com/icons/svg/1661/1661318.svg').attr('id', 'fishTable').attr('x', 370).attr('y', 117);
+		svg.append('image').attr('href', 'https://i1.wp.com/teameverlast.everlast.com/wp-content/uploads/2016/07/2000px-Everlast-logo-2011.svg_.png?resize=300%2C197&ssl=1')
+			.attr('id', 'everlast').attr('x', 95).attr('y', 190);
+        var hammer = svg.append('image').attr('href', 'https://image.flaticon.com/icons/svg/1538/1538118.svg').attr('id', 'hammer').attr('x', 50).attr('y', 330)
+          hammer.call(d3.drag().on("start", d => started(d)));
+
+
+        var saw = svg.append('image').attr('href', 'https://image.flaticon.com/icons/svg/1538/1538124.svg').attr('id', 'saw').attr('x', 150).attr('y', 315)
+           
+        var chainsaw = svg.append('image').attr('href', 'https://image.flaticon.com/icons/svg/123/123935.svg').attr('id', 'chainsaw').attr('x', 250).attr('y', 320)
+           
+        var screws = svg.append('image').attr('href', 'https://image.flaticon.com/icons/svg/289/289690.svg').attr('id', 'screws').attr('x', 350).attr('y', 320)
             
-        svg
-			.append('image')
-			.attr(
-				'href', 'https://image.flaticon.com/icons/svg/1538/1538118.svg'
-			)
-			.attr('id', 'hammer')
-			.attr('x', 50)
-            .attr('y', 330);
+		var tapemeasure = svg.append('image').attr('href', 'https://image.flaticon.com/icons/svg/1589/1589247.svg').attr('id', 'tapemeasure').attr('x', 420).attr('y', 335)
             
-        svg
-			.append('image')
-			.attr(
-				'href', 'https://image.flaticon.com/icons/svg/1538/1538124.svg'
-			)
-			.attr('id', 'saw')
-			.attr('x', 150)
-            .attr('y', 315);
 
-        svg
-			.append('image')
-			.attr(
-				'href', 'https://image.flaticon.com/icons/svg/123/123935.svg'
-			)
-			.attr('id', 'chainsaw')
-			.attr('x', 250)
-            .attr('y', 320);
-
-        svg
-			.append('image')
-			.attr(
-				'href', 'https://image.flaticon.com/icons/svg/289/289690.svg'
-			)
-			.attr('id', 'screws')
-			.attr('x', 350)
-            .attr('y', 320);
-
-        svg
-			.append('image')
-			.attr(
-				'href', 'https://image.flaticon.com/icons/svg/1589/1589247.svg'
-			)
-			.attr('id', 'tapemeasure')
-			.attr('x', 420)
-            .attr('y', 335);
-
-		// d3.select('door').append('rect').attr('class', 'doorOpen');
 		function draw(selection) {
 			var xy0,
 				path,
-				keep = false,
+                keep = false,
+                g = svg.append('g'),
 				line = d3
 					.line()
 					.x(function(d) {
@@ -150,18 +92,17 @@ export default class Garage extends Component {
 					.y(function(d) {
 						return d[1];
 					});
-
+                    
 			selection
 				.on('mousedown', function() {
 					keep = true;
-					xy0 = d3.mouse(this);
+                    xy0 = d3.mouse(this);
 					path = d3
-						.select('svg')
+						.select('g')
 						.append('path')
-						.attr('class', 'containerBar')
+						.attr('class', 'containerMount')
 						.attr('d', line([ xy0, xy0 ]))
-						.style('stroke', 'red')
-						.style('stroke-width', '2px');
+						
 				})
 				.on('mouseup', function() {
 					keep = false;
@@ -175,37 +116,14 @@ export default class Garage extends Component {
 							})
 						]);
 						path.attr('d', Line);
-					}
+                    } 
 				});
 
-			var linkVertical = d3
-				.linkVertical()
-				.x(function(d) {
-					return d.x;
-				})
-				.y(function(d) {
-					return d.y;
-				});
-
-			var linksData = [ { source: { y: 35, x: 110 }, target: { y: 230, x: 110 } } ];
-
+			const linkVertical = d3.linkVertical().x(function(d) {return d.x;}).y(function(d) {return d.y;});
+			const linksData = [ { source: { y: 35, x: 110 }, target: { y: 230, x: 110 } } ];
 			const ellipses = [ { cx: 110, cy: 35, rx: 12.5, ry: 2.5 }, { cx: 110, cy: 200, rx: 37.5, ry: 70 } ];
-
 			const svgEllipses = svg.selectAll('ellipse').data(ellipses).enter().append('ellipse');
-
-			svgEllipses
-				.attr('cx', (d, i) => {
-					return d.cx;
-				})
-				.attr('cy', (d, i) => {
-					return d.cy;
-				})
-				.attr('rx', (d, i) => {
-					return d.rx;
-				})
-				.attr('ry', (d, i) => {
-					return d.ry;
-				});
+			svgEllipses.attr('cx', (d, i) => {return d.cx;}).attr('cy', (d, i) => {return d.cy;}).attr('rx', (d, i) => {return d.rx;}).attr('ry', (d, i) => {return d.ry;});
 
 			svg
 				.selectAll('ellipses')
@@ -213,28 +131,78 @@ export default class Garage extends Component {
 				.enter()
 				.append('path')
 				.attr('stroke', 'black')
-				.attr('d', linkVertical);
-			svgEllipses.filter(':nth-child(3n)').attr('fill', 'tan').attr('stroke', 'black');
-		}
-    }
-    
-    handleChange = (event) => {
-        this.setState({value: event.target.value});
-      }
-    
-    handleSubmit = (event) => {
-        alert('The size of your container is: ' + this.state.value);
-        event.preventDefault();
-      }
-	
-	render() {
-		
+                .attr('d', linkVertical);
+			
+        }
 
+        function started() {
+          
+            
+            var tool = d3.select(this).classed("dragging", true);
+            console.log(tool)
+            d3.event.on("drag", dragged).on("end", ended);
+          
+            function dragged() {
+                console.log('hi')
+            //   tool.raise().attr("x", coords.x = d3.event.x).attr("y", coords.y = d3.event.y);
+            }
+          
+            function ended() {
+              tool.classed("dragging", false);
+            }
+          }
+          
+
+    }
+
+    
+
+
+ 
+
+
+	handleChange = (event) => {
+		this.setState({ value: event.target.value, bottomPoint: !this.state.bottomPoint });
+	};
+
+	handleSubmit = (event) => {
+        event.preventDefault();
+        const newLine = d3.select('.containerMount').node();
+        var l = newLine.getTotalLength();
+        var p = newLine.getPointAtLength([0]);
+        var q = newLine.getPointAtLength(10 * l);
+        var path = d3.path();
+        if (this.state.value === 'small') {
+            path.moveTo(p.x,p.y);
+            path.lineTo(p.x, p.y-(3/4*l) - 25);
+            path.rect(p.x -15, p.y-(3/4*l) - 40, 30, 15);
+            path.moveTo(p.x, p.y-(3/4*l) - 40);
+            path.lineTo(p.x, q.y);
+            d3.select('.containerMount').attr('d', path).style('stroke', '#49fb35');
+        } 
+		if (this.state.value === 'medium') {
+            path.moveTo(p.x,p.y);
+            path.lineTo(p.x, p.y-(1/3*l));
+            path.rect(p.x -25, p.y-(1/3*l) - 50, 50, 50);
+            path.moveTo(p.x, p.y-(1/3*l) - 50);
+            path.lineTo(p.x, q.y);
+            d3.select('.containerMount').attr('d', path).style('stroke', '#49fb35');
+        } 
+        if(this.state.value === 'large') {
+            path.moveTo(p.x,p.y);
+            path.lineTo(p.x, p.y-(2/7*l));
+            path.rect(p.x -50, p.y-(2/7*l) - 40, 100, 40);
+            path.moveTo(p.x, p.y-(2/7*l) - 40);
+            path.lineTo(p.x, q.y);
+            d3.select('.containerMount').attr('d', path).style('stroke', '#49fb35');
+		}
+	};
+
+	render() {
 		return (
 			<form onSubmit={this.handleSubmit}>
-                
 				<label>
-					Choose your container: 
+					Now choose your container:
 					<select value={this.state.value} onChange={this.handleChange}>
 						<option value="small">small</option>
 						<option value="medium">medium</option>
