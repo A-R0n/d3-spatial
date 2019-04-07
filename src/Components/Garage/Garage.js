5cc869c3cfc223bcb7f748b00cb6e242df0d78f5
Aaron Estes <aestesc@gmail.com>
2019-04-07 11:42:52 -0500

    Seperated responsibilities

--- src/Components/Garage/Garage.js
+++ src/Components/Garage/Garage.js
@@ -8,261 +8,196 @@ export default class Garage extends Component {
 		super();
 
 		this.state = {
-			value: 'small',
-			bottomPoint: null,
-			hammer: [ 50, 330, 12 ],
-			saw: [ 150, 315, 30 ],
-			chainsaw: [ 250, 320, 70 ],
-			screws: [ 350, 320, 1.05 ],
-			tapemeasure: [ 420, 335, 8 ],
-            lowerBox: 0,
-            space: 5
+			width: 700,
+			height: 440,
+			margin: { top: 5.714, right: 10, bottom: 5.714, left: 10 },
+			ceilingPoints: [[0,0], [90, 50], [630, 50], [700, 0]],
+			floorPoints: [[0,440], [90, 340], [630, 340], [700, 440]],
+			doorEdgePoints: [[612, 113], [615,114], [615, 322], [612, 326.1]],
+			doorFacePoints: [[612, 327], [540, 310], [540, 116.5],[612, 113], [612, 327]],
+			line: d3.line()
 		};
 	}
 
 	componentDidMount() {
-		this.drawGarage();
+		this.defineTheGarage();
+	};
+
+	defineTheGarage = () => {
+		const {width, height} = this.state;
+		const garage = d3.select('.garage').attr('width', width).attr('height', height);
+		this.defineInnerGarage(garage);	
+	};
+
+	defineInnerGarage = (selection) => {
+		const { width, height, margin } = this.state
+		selection.append('rect').attr('class', 'innerGarage');
+		d3.select('.innerGarage').attr('x', margin.left).attr('y', margin.top).attr('width', width - margin.left - margin.right).attr('height', height - margin.top - margin.bottom);
+		this.createSpacialAwareness(selection);
+	};
+
+	createSpacialAwareness = (selection) => {
+		this.createMainWall(selection);
+		this.createCeiling(selection);
+		this.createFloor(selection);
+		this.createDoorway(selection);
+		this.createDraggability(selection)
+	};
+
+	createMainWall = (selection) => {
+		selection.append('g').append('rect').attr('class', 'mainWall');
+		const wallFacing = d3.select('.mainWall').attr('x', 90).attr('y', 50).attr('width', 540).attr('height', 290);
+		this.createMountGroup(wallFacing);
+	};
+
+	createMountGroup = (selection) => {
+		d3.select('.garage').append('g').attr('class', 'mountGroup');
+		selection.call(this.allowMountDesign);
+	};
+
+	createCeiling = (selection) => {
+		const {ceilingPoints, line} = this.state
+		var pathMountData = line(ceilingPoints);
+		selection.append('path').attr('class', 'ceiling').attr('d', pathMountData);	
+	};
+
+	createFloor = (selection) => {
+		const {floorPoints, line} = this.state;
+		var pathMountData = line(floorPoints);
+		selection.append('path').attr('class', 'floor').attr('d', pathMountData);
+		selection.append('g').attr('class', 'tools');
+	};
+
+	createDoorway = (selection) => {
+		selection.append('rect').attr('class', 'doorWay').attr('x', 520).attr('y', 110).attr('width', 100).attr('height', 225);
+		this.createDoor(selection)
+	};
+
+	createDoor = (selection) => {
+		this.doorEdge(selection)
+		this.doorFace(selection)
+		this.doorHandle(selection)
+	};
+
+	doorEdge = (selection) => {
+		const {doorEdgePoints, line} = this.state;
+		const doorEdge = line(doorEdgePoints);
+		selection.append('path').attr('class', 'doorEdge').attr('d', doorEdge);
 	}
 
-	drawGarage = () => {
-        const {value, bottomPoint, hammer, saw, chainsaw, screws, tapemeasure, lowerBox} = this.state;
-       var space = 0;
-		const margin = { top: 20, right: -20, bottom: 20, left: 0 };
-		const width = 520 - margin.left - margin.right;
-		const height = 460 - margin.top - margin.bottom;
-		var svg = d3.select('.garage').append('svg').attr('class', 'house').attr('width', width).attr('height', height);
-		svg.append('rect').attr('class', 'garage').attr('x', 20).attr('y', 20).attr('width', 500).attr('height', 380);
+	doorFace = (selection) => {
+		const {doorFacePoints, line} = this.state;
+		const doorFace = line(doorFacePoints);
+		selection.append('path').attr('class', 'doorFace').attr('d', doorFace);
+	}
 
-		svg
-			.append('rect')
-			.attr('class', 'squareSpace')
-			.attr('x', 65)
-			.attr('y', 40)
-			.attr('width', 420)
-			.attr('height', 270);
-		d3.select('.squareSpace').call(draw);
+	doorHandle = (selection) => {
+		selection.append('circle').attr('class', 'doorHandle').attr('cx', 550).attr('cy', 230).attr('r', 4.5);
+	}
 
-		svg
-			.append('rect')
-			.attr('class', 'groundTrim')
-			.attr('x', 65)
-			.attr('y', 305)
-			.attr('width', 420)
-			.attr('height', 5);
-		svg.append('rect').attr('class', 'door').attr('x', 370).attr('y', 80).attr('width', 100).attr('height', 225);
+	allowMountDesign = (selection) => {
+		var mouseCoords,
+		circle,
+			pathMount,
+			keepMount = false,
+			lineMount = d3.line().x((d) => d[0]).y((d) => d[1]);
 
-		svg.append('line').attr('class', 'doorOpenEdge').attr('x1', 462).attr('y1', 82).attr('x2', 465).attr('y2', 83);
-		svg.append('line').attr('class', 'doorOpenEdge').attr('x1', 462).attr('y1', 82).attr('x2', 462).attr('y2', 300);
-		svg.append('line').attr('class', 'doorOpenEdge').attr('x1', 465).attr('y1', 83).attr('x2', 465).attr('y2', 293);
-		svg
-			.append('line')
-			.attr('class', 'doorOpenEdge')
-			.attr('x1', 462)
-			.attr('y1', 300)
-			.attr('x2', 465)
-			.attr('y2', 293);
-		svg.append('line').attr('id', 'doorFrame').attr('x1', 415).attr('y1', 88).attr('x2', 415).attr('y2', 265);
-		svg.append('line').attr('id', 'doorFrame').attr('x1', 415).attr('y1', 88).attr('x2', 462).attr('y2', 82);
-		svg.append('line').attr('id', 'doorFrame').attr('x1', 415).attr('y1', 265).attr('x2', 462).attr('y2', 300);
-		svg
-			.append('ellipse')
-			.attr('class', 'doorHandleBase')
-			.attr('cx', 420)
-			.attr('cy', 188)
-			.attr('rx', 2.25)
-			.attr('ry', 5)
-			.attr('fill', '#282828')
-			.attr('stroke', '#282828');
-		svg.append('circle').attr('class', 'doorHandle').attr('cx', 414).attr('cy', 188).attr('r', 3.5);
-		svg.append('line').attr('class', 'topLeft').attr('x1', 20).attr('y1', 20).attr('x2', 65).attr('y2', 40);
-		svg.append('line').attr('class', 'bottomLeft').attr('x1', 20).attr('y1', 400).attr('x2', 65).attr('y2', 310);
-		svg.append('line').attr('class', 'bottomRight').attr('x1', 520).attr('y1', 400).attr('x2', 485).attr('y2', 310);
-		svg.append('line').attr('class', 'topRight').attr('x1', 520).attr('y1', 20).attr('x2', 485).attr('y2', 40);
-		svg.append('line').attr('id', 'backbackdrop').attr('x1', 370).attr('y1', 145).attr('x2', 415).attr('y2', 145);
-
-		svg
-			.append('image')
-			.attr('href', 'https://image.flaticon.com/icons/svg/1519/1519002.svg')
-			.attr('id', 'fishTank')
-			.attr('x', 370)
-			.attr('y', 100);
-		svg
-			.append('image')
-			.attr('href', 'https://image.flaticon.com/icons/svg/1661/1661318.svg')
-			.attr('id', 'fishTable')
-			.attr('x', 370)
-			.attr('y', 117);
-		svg
-			.append('image')
-			.attr(
-				'href',
-				'https://i1.wp.com/teameverlast.everlast.com/wp-content/uploads/2016/07/2000px-Everlast-logo-2011.svg_.png?resize=300%2C197&ssl=1'
-			)
-			.attr('id', 'everlast')
-			.attr('x', 95)
-			.attr('y', 190);
-
-		const tools = svg.append('g').attr('class', 'tools').datum([this.state]);
-		tools
-			.append('image')
-			.attr('href', 'https://image.flaticon.com/icons/svg/1538/1538118.svg')
-			.attr('id', 'hammer')
-			.attr('x', 50)
-			.attr('y', 330)
-			.data([ hammer ])
-			.enter();
-		tools
-			.append('image')
-			.attr('href', 'https://image.flaticon.com/icons/svg/1538/1538124.svg')
-			.attr('id', 'saw')
-			.attr('x', 150)
-			.attr('y', 315)
-			.data([ saw ])
-			.enter();
-		tools
-			.append('image')
-			.attr('href', 'https://image.flaticon.com/icons/svg/123/123935.svg')
-			.attr('id', 'chainsaw')
-			.attr('x', 250)
-			.attr('y', 320)
-			.data([ chainsaw ])
-			.enter();
-		tools
-			.append('image')
-			.attr('href', 'https://image.flaticon.com/icons/svg/289/289690.svg')
-			.attr('id', 'screws')
-			.attr('x', 350)
-			.attr('y', 320)
-			.data([ screws ])
-			.enter();
-		tools
-			.append('image')
-			.attr('href', 'https://image.flaticon.com/icons/svg/1589/1589247.svg')
-			.attr('id', 'tapemeasure')
-			.attr('x', 420)
-			.attr('y', 335)
-			.data([ tapemeasure ])
-            .enter();
-
-		d3.selectAll('#hammer').call(d3.drag().subject({x:50,y:330}).on('start', started).on('drag', dragged).on('end', ended));
-		d3.selectAll('#saw').call(d3.drag().subject({x:150,y:315}).on('start', started).on('drag', dragged).on('end', ended));
-		d3.selectAll('#chainsaw').call(d3.drag().subject({x:250,y:320}).on('start', started).on('drag', dragged).on('end', ended));
-		d3.selectAll('#screws').call(d3.drag().subject({x:350,y:320}).on('start', started).on('drag', dragged).on('end', ended));
-		d3.selectAll('#tapemeasure').call(d3.drag().subject({x:420,y:335}).on('start', started).on('drag', dragged).on('end', ended));
-
-		function draw(selection) {
-			var xy0,
-				path,
-				keep = false,
-				g = svg.append('g'),
-				line = d3.line().x((d) => d[0]).y((d) => d[1]);
-
-			selection
-				.on('mousedown', function() {
-					keep = true;
-					xy0 = d3.mouse(this);
-					path = d3.select('g').append('path').attr('class', 'containerMount').attr('d', line([ xy0, xy0 ]));
-                })
-                .on('mousemove', function() {
-					if (keep) {
-						var Line = line([
-							xy0,
-							d3.mouse(this).map(function(x) {
-								return x - 1;
-							})
-						]);
-						path.attr('d', Line);
-					}
-				})
-				.on('mouseup', function() {
-					keep = false;
-				});
-	const boxingArea = svg.append('g');
-			const bagLink = d3
-				.linkVertical()
-				.x(function(d) {
-					return d.x;
-				})
-				.y(function(d) {
-					return d.y;
-				});
-			const linksData = [ { source: { y: 35, x: 110 }, target: { y: 230, x: 110 } } ];
-			const ellipses = [ { cx: 110, cy: 35, rx: 12.5, ry: 2.5 }, { cx: 110, cy: 200, rx: 37.5, ry: 70 } ];
-			const svgEllipses = boxingArea.selectAll('ellipse').data(ellipses).enter().append('ellipse');
-			svgEllipses
-				.attr('cx', (d, i) => {
-					return d.cx;
-				})
-				.attr('cy', (d, i) => {
-					return d.cy;
-				})
-				.attr('rx', (d, i) => {
-					return d.rx;
-				})
-				.attr('ry', (d, i) => {
-					return d.ry;
-				});
-			boxingArea
-				.selectAll('ellipses')
-				.data(linksData)
-				.enter()
-				.append('path')
-				.attr('stroke', 'black')
-                .attr('d', bagLink);
-            
-            const movingBagPath = d3.path();
-            movingBagPath.arc(110, 220, 45, 300, 240)
-
-                function translateAlong(path) {
-                    var l = path.getTotalLength();
-                    return function(d, i, a) {
-                        return function(t) {
-                            var p = path.getPointAtLength(t * l);
-                            return `translate(${p.x}, ${p.y})`;
-                        };
-                    };
-                }
-
-                function transition() {
-                    d3.select('#boxingBag')
-                        .transition()
-                        .duration()
-                        .attrTween('transform', translateAlong(movingBagPath.node()))
-                        .on('end', transition);
-                }
+		selection
+			.on('mousedown', function() {
+				keepMount = true;
+				mouseCoords = d3.mouse(this);
+				
+				pathMount = d3.select('g.mountGroup').append('path').attr('class', 'containerMount').attr('d', lineMount([ mouseCoords, mouseCoords ]));
+				makeCircle(mouseCoords);
+			})
+			.on('mousemove', function() {
+				if (keepMount) {
+					var Line = lineMount([
+						mouseCoords,
+						d3.mouse(this).map(function(x) {
+							return x - 1;
+						})
+					]);
+					pathMount.attr('d', Line);
+				}
+			})
+			.on('mouseup', function() {
+				keepMount = false;
+			});
+			function makeCircle(mouseCoords){
+				console.log(mouseCoords)
+				d3.select('.garage').append('circle').attr('id', 'startingPoint').attr('cx', mouseCoords[0]).attr('cy', mouseCoords[1]).attr('r', 5);
+			}
 		}
 
-		function started(d) {
+	createDraggability = () => {
+		function started() {
 			d3.select(this).classed('dragging', true);
 			d3.event.on('drag', dragged).on('end', ended);
 		}
-		function dragged(d) {
+		function dragged() {
 			d3.select(this).attr('x',  d3.event.x).attr('y', d3.event.y);
 		}
-
-		function ended(d) {
-			var textInContainer = 
-               d3.select('#space')
-                .data([d])
-                .enter()
-                .exit()
-                .remove();
-
-                d3.select('#space')
-				.text((d) => {
-					return `${parseInt(d[2]) + parseInt(space)}` + `%`;
-                });
-                if(parseInt(d[2]) + parseInt(space) > 100){
-                    alert('Container has exceeded its limit!')
-                    d3.select('#space').attr('fill', 'red');
-                }
-            d3.select(this).style('opacity', 0);
-            space += d[2];
-            d3.select(this).classed('dragging', false);  
+		function ended(volumeTool) {
+			var spaceUsed = 0;
+			var textInContainer = d3.select('#space').data([volumeTool]).enter();
+			d3.select('#space').exit().remove();
+			d3.select('#space').text((volumeTool) => {
+					return `${volumeTool + spaceUsed}` + `%`;
+      	});
+          if(volumeTool + spaceUsed > 100){
+              alert('Container has exceeded its limit!')
+                d3.select('#space').attr('fill', 'red');
+            }
+      d3.select(this).style('opacity', 0);
+      spaceUsed += volumeTool;
+      d3.select(this).classed('dragging', false);  
 		}
-	};
+		this.createDraggableTools(started, dragged, ended);
+	}
 
-	
+	createDraggableTools = (started, dragged, ended) => {
+		this.createHammer(started, dragged, ended);
+		this.createSaw(started, dragged, ended);
+		this.createChainSaw(started, dragged, ended);
+		this.createScrews(started, dragged, ended);
+		this.createTapeMeasure(started, dragged, ended);
+	}
+
+	createHammer = (started, dragged, ended) => {
+		var hammerVolume = [12];
+		d3.select('.tools').append('image').attr('href', 'https://image.flaticon.com/icons/svg/1538/1538118.svg').attr('id', 'hammer').attr('x', 100).attr('y', 370);
+		d3.select('#hammer').data(hammerVolume).enter();
+		d3.select('#hammer').call(d3.drag().subject({x:100,y:370}).on('start', started).on('drag', dragged).on('end', ended));
+	}
+
+	createSaw = (started, dragged, ended) => {
+		var sawVolume = [30];
+		d3.select('.tools').append('image').attr('href', 'https://image.flaticon.com/icons/svg/1538/1538124.svg').attr('id', 'saw').attr('x', 200).attr('y', 355)
+		d3.select('#saw').data(sawVolume).enter();
+		d3.select('#saw').call(d3.drag().subject({x:200,y:355}).on('start', started).on('drag', d => dragged(d)).on('end', ended));
+	}
+
+	createChainSaw = (started, dragged, ended) => {
+		var chainsawVolume = [70];
+		d3.select('.tools').append('image').attr('href', 'https://image.flaticon.com/icons/svg/123/123935.svg').attr('id', 'chainsaw').attr('x', 260).attr('y', 350)
+		d3.select('#chainsaw').data(chainsawVolume).enter();
+		d3.select('#chainsaw').call(d3.drag().subject({x:260,y:350}).on('start', started).on('drag', d => dragged(d)).on('end', ended));
+	}
+
+	createScrews = (started, dragged, ended) => {
+		var screwsVolume = [1.05];
+		d3.select('.tools').append('image').attr('href', 'https://image.flaticon.com/icons/svg/289/289690.svg').attr('id', 'screws').attr('x', 420).attr('y', 360)
+		d3.select('#screws').data(screwsVolume).enter();
+		d3.select('#screws').call(d3.drag().subject({x:420,y:360}).on('start', started).on('drag', d => dragged(d)).on('end', ended));
+	}
+
+	createTapeMeasure = (started, dragged, ended) => {
+		var tmVolume = [8];
+		d3.select('.tools').append('image').attr('href', 'https://image.flaticon.com/icons/svg/1589/1589247.svg').attr('id', 'tapemeasure').attr('x', 520).attr('y', 370)
+		d3.select('#tapemeasure').data(tmVolume).enter();
+		d3.select('#tapemeasure').call(d3.drag().subject({x:520,y:370}).on('start', started).on('drag', d => dragged(d)).on('end', ended));
+	}
 
 	render() {
 		return (
