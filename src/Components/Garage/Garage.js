67e65d1806c2c13d0a0bc9f255aa5891ac5b1f4b
Aaron Estes <aestesc@gmail.com>
2019-04-07 13:13:58 -0500

    Improved readability with consistent formatting

--- src/Components/Garage/Garage.js
+++ src/Components/Garage/Garage.js
@@ -70,30 +70,30 @@ export default class Garage extends Component {
 
 	createDoorway = (selection) => {
 		selection.append('rect').attr('class', 'doorWay').attr('x', 520).attr('y', 110).attr('width', 100).attr('height', 225);
-		this.createDoor(selection)
+		this.createDoor(selection);
 	};
 
 	createDoor = (selection) => {
-		this.doorEdge(selection)
-		this.doorFace(selection)
-		this.doorHandle(selection)
+		this.doorEdge(selection);
+		this.doorFace(selection);
+		this.doorHandle(selection);
 	};
 
 	doorEdge = (selection) => {
 		const {doorEdgePoints, line} = this.state;
 		const doorEdge = line(doorEdgePoints);
 		selection.append('path').attr('class', 'doorEdge').attr('d', doorEdge);
-	}
+	};
 
 	doorFace = (selection) => {
 		const {doorFacePoints, line} = this.state;
 		const doorFace = line(doorFacePoints);
 		selection.append('path').attr('class', 'doorFace').attr('d', doorFace);
-	}
+	};
 
 	doorHandle = (selection) => {
 		selection.append('circle').attr('class', 'doorHandle').attr('cx', 550).attr('cy', 230).attr('r', 4.5);
-	}
+	};
 
 	allowMountDesign = (selection) => {
 		var mouseCoords,
@@ -125,19 +125,18 @@ export default class Garage extends Component {
 				keepMount = false;
 			});
 			function makeCircle(mouseCoords){
-				console.log(mouseCoords)
 				d3.select('.garage').append('circle').attr('id', 'startingPoint').attr('cx', mouseCoords[0]).attr('cy', mouseCoords[1]).attr('r', 5);
-			}
-		}
+			};
+		};
 
 	createDraggability = () => {
 		function started() {
 			d3.select(this).classed('dragging', true);
 			d3.event.on('drag', dragged).on('end', ended);
-		}
+		};
 		function dragged() {
 			d3.select(this).attr('x',  d3.event.x).attr('y', d3.event.y);
-		}
+		};
 		function ended(volumeTool) {
 			var spaceUsed = 0;
 			var textInContainer = d3.select('#space').data([volumeTool]).enter();
@@ -152,9 +151,9 @@ export default class Garage extends Component {
       d3.select(this).style('opacity', 0);
       spaceUsed += volumeTool;
       d3.select(this).classed('dragging', false);  
-		}
+		};
 		this.createDraggableTools(started, dragged, ended);
-	}
+	};
 
 	createDraggableTools = (started, dragged, ended) => {
 		this.createHammer(started, dragged, ended);
@@ -162,42 +161,42 @@ export default class Garage extends Component {
 		this.createChainSaw(started, dragged, ended);
 		this.createScrews(started, dragged, ended);
 		this.createTapeMeasure(started, dragged, ended);
-	}
+	};
 
 	createHammer = (started, dragged, ended) => {
 		var hammerVolume = [12];
 		d3.select('.tools').append('image').attr('href', 'https://image.flaticon.com/icons/svg/1538/1538118.svg').attr('id', 'hammer').attr('x', 100).attr('y', 370);
 		d3.select('#hammer').data(hammerVolume).enter();
 		d3.select('#hammer').call(d3.drag().subject({x:100,y:370}).on('start', started).on('drag', dragged).on('end', ended));
-	}
+	};
 
 	createSaw = (started, dragged, ended) => {
 		var sawVolume = [30];
-		d3.select('.tools').append('image').attr('href', 'https://image.flaticon.com/icons/svg/1538/1538124.svg').attr('id', 'saw').attr('x', 200).attr('y', 355)
+		d3.select('.tools').append('image').attr('href', 'https://image.flaticon.com/icons/svg/1538/1538124.svg').attr('id', 'saw').attr('x', 200).attr('y', 355);
 		d3.select('#saw').data(sawVolume).enter();
 		d3.select('#saw').call(d3.drag().subject({x:200,y:355}).on('start', started).on('drag', d => dragged(d)).on('end', ended));
-	}
+	};
 
 	createChainSaw = (started, dragged, ended) => {
 		var chainsawVolume = [70];
-		d3.select('.tools').append('image').attr('href', 'https://image.flaticon.com/icons/svg/123/123935.svg').attr('id', 'chainsaw').attr('x', 260).attr('y', 350)
+		d3.select('.tools').append('image').attr('href', 'https://image.flaticon.com/icons/svg/123/123935.svg').attr('id', 'chainsaw').attr('x', 260).attr('y', 350);
 		d3.select('#chainsaw').data(chainsawVolume).enter();
 		d3.select('#chainsaw').call(d3.drag().subject({x:260,y:350}).on('start', started).on('drag', d => dragged(d)).on('end', ended));
-	}
+	};
 
 	createScrews = (started, dragged, ended) => {
 		var screwsVolume = [1.05];
-		d3.select('.tools').append('image').attr('href', 'https://image.flaticon.com/icons/svg/289/289690.svg').attr('id', 'screws').attr('x', 420).attr('y', 360)
+		d3.select('.tools').append('image').attr('href', 'https://image.flaticon.com/icons/svg/289/289690.svg').attr('id', 'screws').attr('x', 420).attr('y', 360);
 		d3.select('#screws').data(screwsVolume).enter();
 		d3.select('#screws').call(d3.drag().subject({x:420,y:360}).on('start', started).on('drag', d => dragged(d)).on('end', ended));
-	}
+	};
 
 	createTapeMeasure = (started, dragged, ended) => {
 		var tmVolume = [8];
-		d3.select('.tools').append('image').attr('href', 'https://image.flaticon.com/icons/svg/1589/1589247.svg').attr('id', 'tapemeasure').attr('x', 520).attr('y', 370)
+		d3.select('.tools').append('image').attr('href', 'https://image.flaticon.com/icons/svg/1589/1589247.svg').attr('id', 'tapemeasure').attr('x', 520).attr('y', 370);
 		d3.select('#tapemeasure').data(tmVolume).enter();
 		d3.select('#tapemeasure').call(d3.drag().subject({x:520,y:370}).on('start', started).on('drag', d => dragged(d)).on('end', ended));
-	}
+	};
 
 	render() {
 		return (
@@ -206,5 +205,5 @@ export default class Garage extends Component {
 				<svg className="garage" />
 			</div>
 		);
-	}
-}
+	};
+};
